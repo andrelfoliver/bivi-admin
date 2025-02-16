e785e9971fc6676a769075ecfc68a1e87561d066
bivisualizer <info@bivisualizer.com>
2025-02-15 21:44:30 -0300

    react atualizado

---
+++ frontend/src/TestInput.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default function TestInput() { return ( <div className="min-h-screen flex items-center justify-center bg-gray-200"> <input type="text" name="teste" placeholder="Teste Tailwind" className="p-4 bg-red-500 text-white rounded-lg shadow-lg transform hover:scale-105 transition duration-300" /> </div> ); }
