3073e2abb0b48bf1e9fbc7508a5800c20faca7f3
bivisualizer <info@bivisualizer.com>
2025-02-17 11:48:27 -0300

    login gmail V5

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
+      callbackURL: "https://bivi-empresas-28885d192e15.herokuapp.com/auth/google/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
       // Verifica se o e‑mail é do Gmail
